bb607914d7773961f77f622a02d8cfe20e0bbafe
mdswaley <mdswaley075@gmail.com>
2026-06-10 16:07:02 +0530

    adding JavaMailSender

--- src/test/java/com/mdswaley/project/uber/uberApp/TestContainerConfiguration.java
+++ src/test/java/com/mdswaley/project/uber/uberApp/TestContainerConfiguration.java
@@ -2,14 +2,16 @@ package com.mdswaley.project.uber.uberApp;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.containers.MySQLContainer;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @TestConfiguration
 public class TestContainerConfiguration {
 
+    @Bean
     @ServiceConnection
-    MySQLContainer<?> mySQLContainer() {
-        return new MySQLContainer<>("mysql:latest"); // take mySql container from docker image
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>("postgres:latest");
+    } // take PostgreSql container from docker image
 //        so that we can use dummy database which is similar to original database
-    }
 }
--- src/test/java/com/mdswaley/project/uber/uberApp/UberAppApplicationTests.java
+++ src/test/java/com/mdswaley/project/uber/uberApp/UberAppApplicationTests.java
@@ -1,8 +1,32 @@
 package com.mdswaley.project.uber.uberApp;
 
+import com.mdswaley.project.uber.uberApp.services.EmailSenderService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UberAppApplicationTests {
+    @Autowired
+    private EmailSenderService emailSenderService;
+
+    @Test
+    void contextLoad(){
+        emailSenderService.sendEmail("rupeshkumarpanda221@gmail.com",
+                "Test send mail", "This is first mail.");
+    }
+
+    @Test
+    void contestLoad2(){
+        String[] emails = {"cigof21135@fanchatu.com",
+                "mdswaley075@gmail.com",
+                "zk920943@gmail.com",
+                "rajasonu9438@gmail.com"
+        };
+
+        emailSenderService.sendEmail(emails, "Uber Application Developed by - MD Swaley",
+                "This is a test message for checking mails are send or not.");
+    }
+
 
 }
